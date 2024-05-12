c684ebd13e59ee26f688429fce41a4164a5a5d32
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-05-12 20:22:52 -0300

    Add files via upload

---
+++ att47.cs
@@ -0,0 +1,50 @@
+﻿namespace lista64
+{
+    internal class Program
+    {
+        static void ExibirMatriz(int[,] matriz)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matriz[i,j] + "  ");
+                }
+                Console.WriteLine("\n"); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[,] matriz = new int[3,3];
+            Random r = new Random();
+            int soma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for(int j = 0;j < 10; j++)
+                {
+                    matriz[i, j] = r.Next(10);
+                }
+                
+            }
+
+            for (int i = 0; i < 3;i++)
+            {
+                for (int j = i + 1; j < 3; j++)
+                {
+                    soma += matriz[i, j];
+                }
+            }
+
+            Console.WriteLine("Matriz");
+            ExibirMatriz(matriz);
+            Console.WriteLine($"A soma e : {soma}");
+           
+
+        }
+    }
+}
+    
+
